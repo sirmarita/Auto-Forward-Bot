d0adb743ddb17d5da0fa26476cd4f541a2ed5703
Rowland <edwinmarita12@gmail.com>
2021-09-12 16:13:23 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,12 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi, my work is to edit channel posts and auto forward to any assigned destination"             
 
       #About text
-      ABOUT = """
+      ABOUT = "send /start"
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>👨‍💻 Developer:</b> �Talon
+<b>📢 Channel:</b> Finding Name 
+<b>👥 Group:</b> Ns Finding Name 
